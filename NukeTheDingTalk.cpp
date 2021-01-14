58270afde306274902cf576af3a5dbae4fc1c451
xxq04 <64293473+xxq04@users.noreply.github.com>
2021-01-15 07:16:13 +0800

    加入时间间隔功能

--- NukeTheDingTalk.cpp
+++ NukeTheDingTalk.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 
-string shuazan(string uuid, int count, int status)
+string shuazan(string uuid, int count, int status, double interval)
 {
 	string shell = "curl -G --silent --output NUL https://lv.dingtalk.com/interaction/createLike?uuid=" + uuid + "\&count=" + to_string(count);
 	LPCSTR cshell = shell.c_str();
@@ -13,6 +13,7 @@ string shuazan(string uuid, int count, int status)
 	{
 		WinExec(cshell, SW_NORMAL);
 		cout << "正在刷赞---请求数:已经执行" << i + 1 << "次,一共" << status << "次 请求量：" << (i + 1) * count << endl;
+		Sleep(interval);
 	}
 	return "执行成功";
 }
@@ -20,5 +21,5 @@ string shuazan(string uuid, int count, int status)
 int main(int argc, char **argv)
 {
 	SetConsoleOutputCP(65001);
-	shuazan(argv[1],atoi(argv[2]),atoi(argv[3]));
+	shuazan(argv[1],atoi(argv[2]),atoi(argv[3]), atof(argv[4]));
 }
