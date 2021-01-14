ed094f9390aaa08abca42266824b13bb63097a4a
Ubuntu <kangxin2017@outlook.com>
2021-01-14 20:08:52 +0800

    C++版本 	new file:   NukeTheDingTalk.cpp 	deleted:    curl.exe 	deleted:    start.bat 	deleted:    "\351\243\237\347\224\250\345\260\217\346\212\200\345\267\247--\346\212\212\346\255\244\346\226\207\344\273\266\345\221\275\345\220\215\344\270\272fast\345\217\257\345\274\200\345\220\257\345\244\232\347\272\277\347\250\213\345\223\246"

---
+++ NukeTheDingTalk.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+
+string shuazan(string uuid, int count, int status)
+{
+	string shell = "curl -G --silent --output NUL https://lv.dingtalk.com/interaction/createLike?uuid=" + uuid + "\&count=" + to_string(count);
+	LPCSTR cshell = shell.c_str();
+	for (int i = 0; i <= status; i++)
+	{
+		WinExec(cshell, SW_NORMAL);
+		cout << "正在刷赞---请求数:已经执行" << i + 1 << "次,一共" << status << "次 请求量：" << (i + 1) * count << endl;
+	}
+	return "执行成功";
+}
+
+int main(int argc, char **argv)
+{
+	SetConsoleOutputCP(936);
+	shuazan(argv[1],atoi(argv[2]),atoi(argv[3]));
+}
