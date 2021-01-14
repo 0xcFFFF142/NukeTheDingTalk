65621763bdf83477c72681e17f0e3a5d09ef9943
Ubuntu <kangxin2017@outlook.com>
2021-01-14 21:58:46 +0800

    modified:   NukeTheDingTalk.cpp

--- NukeTheDingTalk.cpp
+++ NukeTheDingTalk.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+﻿#pragma execution_character_set("utf-8")
+#include <iostream>
 #include <string>
 #include <windows.h>
 using namespace std;
@@ -8,7 +9,7 @@ string shuazan(string uuid, int count, int status)
 {
 	string shell = "curl -G --silent --output NUL https://lv.dingtalk.com/interaction/createLike?uuid=" + uuid + "\&count=" + to_string(count);
 	LPCSTR cshell = shell.c_str();
-	for (int i = 0; i <= status; i++)
+	for (int i = 0; i < status; i++)
 	{
 		WinExec(cshell, SW_NORMAL);
 		cout << "正在刷赞---请求数:已经执行" << i + 1 << "次,一共" << status << "次 请求量：" << (i + 1) * count << endl;
@@ -18,6 +19,6 @@ string shuazan(string uuid, int count, int status)
 
 int main(int argc, char **argv)
 {
-	SetConsoleOutputCP(936);
+	SetConsoleOutputCP(65001);
 	shuazan(argv[1],atoi(argv[2]),atoi(argv[3]));
 }
