9771b60ba870852c9d4b38375e6d64b13001d41b
xxq04 <64293473+xxq04@users.noreply.github.com>
2021-01-15 10:56:27 +0800

    加入无穷模式

--- NukeTheDingTalk.cpp
+++ NukeTheDingTalk.cpp
@@ -5,15 +5,27 @@
 using namespace std;
 
 
-string shuazan(string uuid, int count, int status, double interval)
+string shuazan(string uuid, int count, int status, double interval, int Farewellmode)
 {
 	string shell = "curl -G --silent --output NUL https://lv.dingtalk.com/interaction/createLike?uuid=" + uuid + "\&count=" + to_string(count);
 	LPCSTR cshell = shell.c_str();
-	for (int i = 0; i < status; i++)
+	if (Farewellmode == 0)
 	{
-		WinExec(cshell, SW_NORMAL);
-		cout << "正在刷赞---请求数:已经执行" << i + 1 << "次,一共" << status << "次 请求量：" << (i + 1) * count << endl;
-		Sleep(interval);
+		for (int i = 0; i < status; i++)
+		{
+			WinExec(cshell, SW_NORMAL);
+			cout << "正在刷赞---请求数:" << i + 1 << "/" << status << "次" << " 请求量：" << count << "*" << (i + 1) << "=" << (i + 1) * count << " UUID:" << uuid << endl;
+			Sleep(interval);
+		}
+	}
+	else if (Farewellmode == 1)
+	{
+		for (int i = 0;;i++) 
+		{
+			WinExec(cshell, SW_NORMAL);
+			cout << "正在刷赞---请求数:" << i + 1 << "/∞次" << " 请求量：" << count<<"*"<<(i+1)<<"="<<(i + 1) * count << " UUID:" << uuid << endl;
+			Sleep(interval);
+		}
 	}
 	return "执行成功";
 }
@@ -21,5 +33,5 @@ string shuazan(string uuid, int count, int status, double interval)
 int main(int argc, char **argv)
 {
 	SetConsoleOutputCP(65001);
-	shuazan(argv[1],atoi(argv[2]),atoi(argv[3]), atof(argv[4]));
+	shuazan(argv[1],atoi(argv[2]),atoi(argv[3]), atof(argv[4]), atoi(argv[5]));
 }
