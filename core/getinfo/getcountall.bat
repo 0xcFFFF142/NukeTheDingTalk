063e3efd8a3c2203d55c83b9b33621d4a5a21146
Ubuntu <kangxin2017@outlook.com>
2021-01-15 10:23:40 +0800

    Version 1.2.3 - Fixed BUG 	modified:   core/getinfo/getcountall.bat 	modified:   start.bat

--- core/getinfo/getcountall.bat
+++ core/getinfo/getcountall.bat
@@ -5,11 +5,11 @@
 	echo 每次请求的数量=%countall%
 	echo.
 			if /i %countall% LEQ 10000000   goto end
-			goto i2
+
 			echo 超过一千万的数字无效,请重新输入一个小于10000000的数字.
 			echo 按下回车自动设置为10000000(1千万)
 			set countall=10000000
 			set /p countall=每次请求的数量:
-
+			goto i2
 :end
 
