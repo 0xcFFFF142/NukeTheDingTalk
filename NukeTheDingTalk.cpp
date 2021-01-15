c7020ae2d821d36303a254b08d903048d3d0581d
xxq04 <64293473+xxq04@users.noreply.github.com>
2021-01-15 11:29:15 +0800

    简化参数

--- NukeTheDingTalk.cpp
+++ NukeTheDingTalk.cpp
@@ -5,20 +5,20 @@
 using namespace std;
 
 
-string shuazan(string uuid, int count, int status, double interval, int Farewellmode)
+string shuazan(string uuid, int count, string status, double interval)
 {
 	string shell = "curl -G --silent --output NUL https://lv.dingtalk.com/interaction/createLike?uuid=" + uuid + "\&count=" + to_string(count);
 	LPCSTR cshell = shell.c_str();
-	if (Farewellmode == 0)
+	if (status != "a")
 	{
-		for (int i = 0; i < status; i++)
+		for (int i = 0; i < stoi(status); i++)
 		{
 			WinExec(cshell, SW_NORMAL);
 			cout << "正在刷赞---请求数:" << i + 1 << "/" << status << "次" << " 请求量：" << count << "*" << (i + 1) << "=" << (i + 1) * count << " UUID:" << uuid << endl;
 			Sleep(interval);
 		}
 	}
-	else if (Farewellmode == 1)
+	else if (status == "a")
 	{
 		for (int i = 0;;i++) 
 		{
@@ -27,11 +27,10 @@ string shuazan(string uuid, int count, int status, double interval, int Farewell
 			Sleep(interval);
 		}
 	}
-	return "执行成功";
 }
 
 int main(int argc, char **argv)
 {
 	SetConsoleOutputCP(65001);
-	shuazan(argv[1],atoi(argv[2]),atoi(argv[3]), atof(argv[4]), atoi(argv[5]));
+	shuazan(argv[1],atoi(argv[2]),argv[3], atof(argv[4]));
 }
