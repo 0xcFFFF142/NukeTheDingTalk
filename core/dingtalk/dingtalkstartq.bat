4fdf549783d942dd9612f915e926d29cc72fca81
Ubuntu <kangxin2017@outlook.com>
2021-01-15 10:00:02 +0800

    modified:   core/apis/api-cpp.exe 	deleted:    core/apis/api-py.exe 	modified:   core/dingtalk/dingtalkstartq.bat 	modified:   core/getinfo/getfastmode.bat 	modified:   start.bat

--- core/dingtalk/dingtalkstartq.bat
+++ core/dingtalk/dingtalkstartq.bat
@@ -1,6 +1,7 @@
-	if /i %ta% == 0 echo 即将调用来自@Xuxiuqi2020的API(C++版本) && api-cpp.exe "%uuid%" %countall% %count% %ta% && goto cppend
-	echo 即将调用来自@Xuxiuqi2020的API(Python版本)
-	api-py.exe "%uuid%" %countall% %count% %ta%
+::	if /i %ta% == 0 
+	echo 即将调用来自@Xuxiuqi2020的API(C++版本) && api-cpp.exe "%uuid%" %countall% %count% %ta% && goto cppend
+::	echo 即将调用来自@Xuxiuqi2020的API(Python版本)
+::	api-py.exe "%uuid%" %countall% %count% %ta%
 	:cppend
 	set start=%count% 
 			set start=1
